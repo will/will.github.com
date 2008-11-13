d21511a41a964974659ec0ad040e101ca8f30353
Will Leinweber <will@bitfission.com>
2008-11-12 19:57:51 -0600

    layout tweaks, added next and previous posts to each page

---
+++ lib/helpers.rb
@@ -0,0 +1,35 @@
+def first_post
+  all_posts.first
+end
+
+def next_post(post)
+  all_posts[ all_posts.rindex(post) - 1 ]
+end
+
+def prev_post(post)
+  all_posts[ all_posts.rindex(post) + 1 ]
+end
+
+def all_posts
+  @all_pages ||= @pages.find( :limit => :all,
+                              :draft => nil,
+                              :sort_by => "created_at",
+                              :blog_post => true,
+                              :reverse => true
+                            )
+end
+
+def other_posts(post=nil)
+  post ||= @page
+  npost = next_post(post)
+  ppost = prev_post(post)
+  html = "<div class='otherposts'><ul>"
+  html += "<li>&nbsp;&nbsp;&nbsp;&nbsp;Next Post: #{link_to_post npost}</li>" if npost unless post == first_post
+  html += "<li>Previous Post: #{link_to_post ppost }</li>" if ppost
+  html += "</ul></div>"
+end
+
+def link_to_post(post)
+  post = @pages.find( :limit =>1, :title => post).first unless post.class == Webby::Resources::Page
+  link_to post.title, post.url
+end
